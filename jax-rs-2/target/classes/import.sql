e34b95489f3fd7101ea210d0e758ab6b37cd3daf
Anatoly Laptev <Anatoly.Laptev@lanit-tercom.ru>
2023-10-15 16:28:47 +0300

    Revert "Реорганизация общего модуля"
    This reverts commit 893f69bdb6f5a0781c9362a541ca2c1f0778c0e1.

--- jax-rs-2/target/classes/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO customer (id, name, surname) VALUES ( nextval('customerId_seq'), 'Homer','Simpson');
-INSERT INTO customer (id, name, surname) VALUES ( nextval('customerId_seq'), 'Bart','Simpson');
-
