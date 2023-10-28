efc35afd7afa6aa5d5afaa688f536f3782cb6eba
Anatoly Laptev <Anatoly.Laptev@lanit-tercom.ru>
2023-10-28 18:38:58 +0300

    обновлены сущности

--- jax-rs-1/src/main/java/com/mastertheboss/jaxrs/domain/entity/Customer.java
+++ jax-rs-1/src/main/java/com/mastertheboss/jaxrs/domain/entity/Customer.java
@@ -32,28 +32,4 @@ public class Customer {
 	@Column(length = 40)
 	private String surname;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getSurname() {
-		return surname;
-	}
-
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
-
 }
