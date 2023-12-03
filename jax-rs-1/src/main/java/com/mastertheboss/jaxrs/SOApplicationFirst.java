35450b7a11e20612a98017995200bd520549cecc
Anatoly <Anatoly.Laptev@lanit-tercom.ru>
2023-12-03 18:31:21 +0300

    fix EJB

--- jax-rs-1/src/main/java/com/mastertheboss/jaxrs/SOApplicationFirst.java
+++ jax-rs-1/src/main/java/com/mastertheboss/jaxrs/SOApplicationFirst.java
@@ -1,8 +1,13 @@
 package com.mastertheboss.jaxrs;
 
 
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 import javax.ws.rs.ApplicationPath;
 
@@ -10,8 +15,23 @@ import javax.ws.rs.ApplicationPath;
 @ApplicationPath("/dragon")
 public class SOApplicationFirst {
 
+	@Value("${http.port}")
+	private int httpPort;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SOApplicationFirst.class, args);
 	}
 
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+		return tomcat;
+	}
+
+	private Connector createStandardConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setPort(httpPort);
+		return connector;
+	}
 }
