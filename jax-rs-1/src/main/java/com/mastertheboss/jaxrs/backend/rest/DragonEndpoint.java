5133553faf1b23074f159d4f5d691c5df6429815
Anatoly <Anatoly.Laptev@lanit-tercom.ru>
2023-12-04 16:57:35 +0300

    fix all

--- jax-rs-1/src/main/java/com/mastertheboss/jaxrs/backend/rest/DragonEndpoint.java
+++ jax-rs-1/src/main/java/com/mastertheboss/jaxrs/backend/rest/DragonEndpoint.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "jax-rs-1/dragon/dragons", produces = MediaType.APPLICATION_JSON_VALUE)
--- jax-rs-1/src/main/java/com/mastertheboss/jaxrs/backend/rest/PersonEndpoint.java
+++ jax-rs-1/src/main/java/com/mastertheboss/jaxrs/backend/rest/PersonEndpoint.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import java.util.List;
 import java.util.Objects;
 
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "jax-rs-1/dragon/persons", produces = MediaType.APPLICATION_JSON_VALUE)
